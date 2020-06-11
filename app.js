0570dd4ecb367481e4dbdc390a1417b4cdbe0278
Davee Sok <daveesok@gmail.com>
2020-06-11 10:56:26 -0700

    initialized

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 'use strict';
 
 console.log('hello');
+
+
+// *****  Display images *******
+
+
+// receive the data
+
+
+// constructor function (take in data and create objects)
+
+
+// ajax call
+
+
+// display the data
+
+
+// create render funtion
+
+
+
+
+
+
+
+
+
+// filter by keyword
+
+
+
+
+
+// sort by horns or name
+
+
+
+
+// call on function to intialize page
